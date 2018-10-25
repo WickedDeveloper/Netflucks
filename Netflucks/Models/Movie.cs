4b71d5ad7b27e8f0c97398d4819524b55a4fb997
Dave Gwyn <gwyn@dgfprofessionalservices.com>
2018-10-25 15:02:48 -0400

    Previous Commits

---
+++ Netflucks/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Netflucks.Models
+{
+    public partial class Movie
+    {
+        public Movie()
+        {
+            Comment = new HashSet<Comment>();
+        }
+
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public int? Rating { get; set; }
+
+        public ICollection<Comment> Comment { get; set; }
+    }
+}
